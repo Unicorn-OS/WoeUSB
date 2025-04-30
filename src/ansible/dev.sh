69e9c9491f745a4073ad1c402dc39817062b5886
Source Graph A.I. <Source.Graph@unisocial.net>
2025-04-30 16:36:17 -0500

    fix dev.sh

--- src/ansible/dev.sh
+++ src/ansible/dev.sh
@@ -1,6 +1,6 @@
-mkdir -p roles/dev
-cd roles/dev 
+mkdir -p roles/dev 
 echo "*/" > roles/dev/.gitignore
 
+cd roles/dev
 git clone git@github.com:Unicorn-OS/ansible-role-WoeUSB-ng.git
 git clone git@github.com:Unicorn-OS/ansible-role-WoeUSB.git
